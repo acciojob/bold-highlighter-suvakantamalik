880743773cbb08ec48051c04a83bb87b8219b363
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-04 17:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let hText = document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
-
+	hText.forEach(text => {
+		text.style.color = "green";
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	hText.forEach(text => {
+		text.style.color = "black";
+	});
 }
